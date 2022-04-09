13ebcf42bec64c5de7d31866e146160c64fd6e15
Amir Kerimov <95amirk@gmail.com>
2022-04-09 18:18:25 +0300

    feat: Add card like switching and card deleting

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,11 @@ import React, { useContext } from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 
-const Card = ({card, onCardClick}) => {
+const Card = ({card, onCardClick, onCardLike, onCardDelete}) => {
   const cardClickHandler = () => onCardClick(card);
+  const cardLikeHandler = () => onCardLike(card);
+  const cardDeleteHadnler = () => onCardDelete(card._id);
+  
   const currentUser = useContext(CurrentUserContext);
 
   const isOwn = currentUser._id === card.owner._id;
@@ -14,14 +17,14 @@ const Card = ({card, onCardClick}) => {
     <li className="element">
       <div className="element__image" style={{ backgroundImage: `url(${card.link})` }} onClick={cardClickHandler} />
       {
-        isOwn && (<button type="button" aria-label="удалить" className="button element__delete-button"></button>)
+        isOwn && (<button onClick={cardDeleteHadnler} type="button" aria-label="удалить" className="button element__delete-button"></button>)
       }
       <div className="element__info">
         <h2 className="element__title">
           {card.name}
         </h2>
         <div className="element__like-container">
-          <button type="button" aria-label="лайк" className={likeBtnClass}>
+          <button onClick={cardLikeHandler} type="button" aria-label="лайк" className={likeBtnClass}>
           </button>
           <p className="element__like-count">{card.likes.length}</p>
         </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import Api from '../utils/api';
+import api from "../utils/api";
 import Card from "./Card";
 
 export default function Main({onEditAvatar, onEditProfile, onAddPlace, onCardClick}) {
@@ -8,8 +8,27 @@ export default function Main({onEditAvatar, onEditProfile, onAddPlace, onCardCli
   const [cards, setCards] = useState([]);
   const currentUser = useContext(CurrentUserContext);
 
+  const handleCardLike = card => {
+    const isLiked = card.likes.some(user => user._id === currentUser._id);
+    if (!isLiked) {
+      api.setLike(card._id)
+        .then(card => setCards(cards.map(item => item._id === card._id ? card : item)))
+        .catch(e => console.log(e))
+    } else {
+      api.deleteLike(card._id)
+        .then(card => setCards(cards.map(item => item._id === card._id ? card : item)))
+        .catch(e => console.log(e))
+    }
+  }
+
+  const handleCardDelete = id => {
+    api.deleteCard(id)
+      .then(() => setCards(cards.filter(card => card._id !== id)))
+      .then(e => console.log(e))
+  }
+
   useEffect(() => {
-    Api.getCards()
+    api.getCards()
       .then(cards => {
         setCards(cards)
       })
@@ -39,7 +58,15 @@ export default function Main({onEditAvatar, onEditProfile, onAddPlace, onCardCli
       <section className="elements"> 
         <ul className="elements__list">
           {
-            cards.map(item => (<Card key={item._id} card={item} onCardClick={onCardClick} />))
+            cards.map(item => (
+              <Card 
+                key={item._id} 
+                card={item} 
+                onCardClick={onCardClick} 
+                onCardLike={handleCardLike}
+                onCardDelete={handleCardDelete}
+              />
+            ))
           }
         </ul>
       </section>
